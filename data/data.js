4593b144460896bfffc4d5239dfda0d3fe1f8a2a
Kevin Villegas <85034143+Namev4@users.noreply.github.com>
2022-09-10 10:54:41 -0300

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    question: "¿Cuál de las siguientes opciones no es un editor de codigo?",
+    choices: ["Vim", "VSCode", "Emacs", "Word"],
+    answer: "Word",
+  },
+  {
+    question: "¿Qué lenguaje no es orientado a Objetos?",
+    choices: ["Java", "Haskell", "C++", "Python"],
+    answer: "Haskell",
+  },
+  {
+    question: "¿Qué lenguaje no sirve para estilizar sitios web?",
+    choices: ["Stylus", "Less", "Sass", "CSS", 'PostCSS', 'NextCSS'],
+    answer: "NextCSS",
+  },
+];
